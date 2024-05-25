af9c1da328a18c31ebad2c4b8f686b931a443b70
JordanNiphan <slaet.gustave@gmail.com>
2024-05-25 08:30:29 -0700

    oiien

--- keyboards/zsa/moonlander/keymaps/Slaet/keymap.c
+++ keyboards/zsa/moonlander/keymaps/Slaet/keymap.c
@@ -13,42 +13,45 @@ enum tap_dance {
     Dance_Esc_Alt,
 };
 
+enum custom_keycodes {
+    SS_ONREADY = SAFE_RANGE,
+    SS_V2Zero = SAFE_RANGE,
+};
+
 enum layers {
     _ColemakDHm,
     _Numbers,
     _Arrows,
     _Planetside,
     _MiscGame,
+    _Godot,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ColemakDHm] = LAYOUT_moonlander(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,               KC_4,               KC_5,           TT(_Planetside),                 KC_MS_WH_UP,    KC_MS_BTN3,     KC_MS_BTN2,     DM_PLY1,        DM_PLY2,            KC_TRANSPARENT, KC_MINUS,
-    KC_TAB,         KC_Q,           KC_W,           KC_F,               KC_P,               KC_B,           TT(_MiscGame),                   KC_MS_BTN1,     KC_J,           KC_L,           KC_U,           KC_Y,               KC_SCLN,        KC_BSLS,
-    KC_MS_BTN2,     KC_A,           KC_R,           KC_S,               KC_T,               KC_G,           KC_TRANSPARENT,                  KC_MS_WH_DOWN,  KC_M,           KC_N,           KC_E,           LT(_Arrows,KC_I),   KC_O,           MT(MOD_RCTL, KC_QUOTE),
-    KC_F17,         KC_Z,           KC_X,           KC_C,               KC_D,               KC_V,                                                            KC_K,           KC_H,           KC_COMMA,       KC_DOT,             KC_SLASH,       OSM(MOD_RSFT),
-    KC_ENTER,       KC_HASH,        KC_SPACE,       KC_MS_WH_DOWN,      KC_MS_WH_UP,                        KC_LEFT_GUI,                    KC_NO,                           KC_RIGHT_ALT,   DM_REC1,        DM_REC2,            KC_TRANSPARENT, DM_RSTP,
-
+    KC_ESCAPE,      KC_1,           KC_2,             KC_3,               KC_4,               KC_5,           TT(_Planetside),                 KC_MS_WH_UP,    KC_MS_BTN3,     KC_MS_BTN2,     KC_TRANSPARENT,    KC_TRANSPARENT,     KC_TRANSPARENT, KC_MINUS,
+    KC_TAB,         KC_Q,           KC_W,             KC_F,               KC_P,               KC_B,           TT(_MiscGame),                   KC_MS_BTN1,     KC_J,           KC_L,           KC_U,              KC_Y,               KC_SCLN,        KC_BSLS,
+    KC_MS_BTN2,     KC_A,           LT(_Godot,KC_R),  KC_S,               KC_T,               KC_G,           KC_TRANSPARENT,                  KC_MS_WH_DOWN,  KC_M,           KC_N,           KC_E,              LT(_Arrows,KC_I),   KC_O,           MT(MOD_RCTL, KC_QUOTE),
+    KC_F17,         KC_Z,           KC_X,             KC_C,               KC_D,               KC_V,                                                            KC_K,           KC_H,           KC_COMMA,          KC_DOT,             KC_SLASH,       OSM(MOD_RSFT),
+    KC_ENTER,       KC_HASH,        KC_SPACE,         KC_MS_WH_DOWN,      KC_MS_WH_UP,                        KC_LEFT_GUI,                  KC_NO,                             KC_RIGHT_ALT,   KC_TRANSPARENT,    KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                         KC_LEFT_CTRL, LT(_Numbers,KC_SPACE), OSM(MOD_LSFT),                 KC_NO,  KC_NO,  KC_NO
   ),
 
   [_Numbers] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_ASTR,        KC_EQUAL,
-    KC_LEFT_SHIFT,  KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_DOT,         KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_MINUS,
-    KC_TRANSPARENT, KC_SLASH,       KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,                                                                        KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_BSLS,        KC_AMPR,
+    KC_LEFT_SHIFT,  KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_DOT,         KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_UNDS,        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_MINUS,
+    KC_TRANSPARENT, KC_SLASH,       KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,                                                                        KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_BSLS,        KC_AMPR,
     KC_TRANSPARENT, KC_COMMA,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT,                 KC_TRANSPARENT,                                 KC_0,           KC_0,           KC_DOT,         KC_EQUAL,       TT(_ColemakDHm),
-
                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [_Arrows] = LAYOUT_moonlander(
-    RGB_HUD,        RGB_SAD,            RGB_MODE_BREATHE,   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,         RGB_SPI,                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    RGB_MOD,        KC_TRANSPARENT,     KC_PGDN,            KC_UP,          KC_PAGE_UP,     KC_TRANSPARENT,         RGB_SPD,                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,         KC_TRANSPARENT,             KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT,     KC_PGDN,            KC_UP,          KC_PAGE_UP,     KC_TRANSPARENT,         KC_TRANSPARENT,             KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, LGUI(LCTL(KC_LEFT)),KC_LEFT,            KC_DOWN,        KC_RIGHT,       LGUI(LCTL(KC_RIGHT)),   KC_TRANSPARENT,             KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    QK_BOOT,        KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT,                         KC_TRANSPARENT,             KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TT(_ColemakDHm),
-
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,            KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    QK_BOOT,        KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT,                         KC_TRANSPARENT,             KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,             KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [_Planetside] = LAYOUT_moonlander(
     TD(Dance_Esc_Alt),  TD(Dance_1_F1),     TD(Dance_2_F2),     TD(Dance_3_F3),     TD(Dance_4_F4), KC_5,           KC_6,                           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -56,7 +59,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LEFT_SHIFT,      KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, TD(Dance_8_H),                  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_LEFT_CTRL,       KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT,                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_CAPS,            KC_KP_7,            KC_TRANSPARENT,     KC_J,               KC_MS_BTN1,                     TD(Dance_M_U),                  KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TT(_ColemakDHm),
-
                                                                                             KC_SPACE,   KC_MS_BTN2, TD(Dance_P_O),                  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [_MiscGame] = LAYOUT_moonlander(
@@ -65,10 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LEFT_SHIFT,  KC_TRANSPARENT, KC_S,           KC_D,           KC_F,           KC_TRANSPARENT, KC_8,                                           KC_TRANSPARENT, KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_TRANSPARENT,
     KC_LEFT_CTRL,   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_V,           KC_B,                                                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_TRANSPARENT, KC_TRANSPARENT,
     KC_NO,          KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                     KC_LEFT_GUI,                KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TT(_ColemakDHm),
-
                                                                         KC_SPACE,       KC_SPACE,       KC_TRANSPARENT,             KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
+  [_Godot] = LAYOUT_moonlander(
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, SS_ONREADY,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_EQUAL,       KC_UNDS,        KC_TRANSPARENT, KC_TRANSPARENT,                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, SS_V2Zero,      KC_TRANSPARENT, KC_TRANSPARENT,                                                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                       KC_TRANSPARENT,            KC_NO,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,            KC_NO,  KC_NO,  KC_NO
+  ),
 };
+
+
 
 const uint16_t PROGMEM combo_bspc[] = { KC_N, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_enter[] = { KC_H, KC_COMMA, COMBO_END};
@@ -78,6 +89,9 @@ const uint16_t PROGMEM combo_quote[] = { KC_L, KC_U, COMBO_END};
 const uint16_t PROGMEM combo_Rmouse[] = { KC_R, KC_S, COMBO_END};
 const uint16_t PROGMEM combo_Mmouse[] = { KC_R, KC_S, KC_T, COMBO_END};
 const uint16_t PROGMEM combo_Lmouse[] = { KC_T, KC_S, COMBO_END};
+const uint16_t PROGMEM combo_equal[] = { KC_U, KC_Y, COMBO_END};
+const uint16_t PROGMEM combo_underscore[] = { KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_bootloader[] = { KC_1, KC_MINUS, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_bspc, KC_BSPC),
@@ -88,6 +102,9 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_Rmouse, KC_MS_BTN2),
     COMBO(combo_Mmouse, KC_MS_BTN3),
     COMBO(combo_Lmouse, KC_MS_BTN1),
+    COMBO(combo_equal, KC_EQUAL),
+    COMBO(combo_underscore, KC_UNDS),
+    COMBO(combo_bootloader, QK_BOOT),
 };
 
 tap_dance_action_t tap_dance_actions[] = {
@@ -99,4 +116,22 @@ tap_dance_action_t tap_dance_actions[] = {
         [Dance_8_H] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_H),
         [Dance_M_U] = ACTION_TAP_DANCE_DOUBLE(KC_M, KC_U),
         [Dance_P_O] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_O),
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SS_ONREADY:
+      if (record->event.pressed) {
+        SEND_STRING("@onready var =");
+      }
+      break;
+
+    case SS_V2Zero:
+      if (record->event.pressed) {
+        SEND_STRING("@onready var =");
+      } 
+      break;
+  }
+  return true;
 };
---
+++ test.py
@@ -0,0 +1,5 @@
+path = 2
+
+while path >  0:
+    print(path)
+    path -= 1
